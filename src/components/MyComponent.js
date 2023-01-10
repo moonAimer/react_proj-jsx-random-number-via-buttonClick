b793a0720829d1559e5a28a603a5438812a6e624
Daniil SkyAimer <bezuglich7@gmail.com>
2023-01-10 16:48:15 +0400

    add state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,4 +1,10 @@
-function MyComponent() {
+import { useState } from "react";
+
+function MyComponent({ initialChangeN }) {
+  const [ChangeNumber, setInitialNumber] = useState(initialChangeN);
+  console.log(ChangeNumber);
+
+  const changeN = () => {};
   return (
     <div>
       <h1>100</h1>
